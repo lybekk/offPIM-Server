43d47332a654ab26a1b96a8053fabce81bd058d4
Christoffer Lybekk <christoffer@lybekk.tech>
2020-07-25 12:31:18 +0200

    early test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # offPIM-Server
 offPIM Server for easy Synchronization
+
+## Tips
+
+Enable CORS in the admin interface
--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log('Booting offPIM Server')
 
+var PouchDB = require('pouchdb');
+var cors = require('cors');
 var express = require('express');
 const path = require('path');
-
 
 /*
  * Settings
@@ -16,18 +17,8 @@ const listeningPortPouchDBServer = 3000;
  */
 var app = express();
 
-app.use('/', require('express-pouchdb')(require('pouchdb'), {
-    mode: 'fullCouchDB', // specified for clarity. It's the default so not necessary.
-    overrideMode: {
-        exclude: [
-            'routes/authentication',
-            // disabling the above, gives error messages which require you to disable the
-            // following parts too. Which makes sense since they depend on it.
-            'routes/authorization',
-            'routes/session'
-        ]
-    }
-}));
+
+app.use('/', require('express-pouchdb')(PouchDB));
 
 app.listen(listeningPortPouchDBServer);
 
