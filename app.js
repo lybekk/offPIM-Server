447f24e141871185ad559b91ff398515027fc19b
lybekk <christoffer@lybekk.tech>
2020-07-30 04:58:13 +0200

    ui update and db actions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var PouchDB = require('pouchdb');
 var cors = require('cors');
 var express = require('express');
 const path = require('path');
+const open = require('open');
 
 /*
  * Settings
@@ -11,26 +12,39 @@ const path = require('path');
 
 const listeningPortoffPIMUI = 3001;
 const listeningPortPouchDBServer = 3000;
+const configuredBindAddress = '127.0.0.1';
+const appURLBase = 'http://localhost:';
 
 /*
- * Server
+ * PouchDB Exress 
+ * dbServer
  */
 var app = express();
-
-
 app.use('/', require('express-pouchdb')(PouchDB));
+var dbServer,
+    dbServerURL;
 
-app.listen(listeningPortPouchDBServer);
+async function dbServerStart() {
+    dbServerURL = appURLBase + listeningPortPouchDBServer;
+    dbServer = await app.listen(listeningPortPouchDBServer, configuredBindAddress);
+    return true
+}
 
-const appURLBase = 'http://localhost:';
-const appURL = appURLBase + listeningPortPouchDBServer;
+async function dbServerStop(restart = false) {
+    await dbServer.close();
+    if (restart) {
+        await dbServerStart()
+    }
+    return true
+}
 
-console.log(`PouchDB Server is available on ${appURL}`,
-    `\n * Visit ${appURL}/_utils for administration`)
+console.log(`PouchDB Server is available on ${dbServerURL}`,
+    `\n * Visit ${dbServerURL}/_utils for administration`)
 
-/* 
-Friendly interface
-*/
+/*
+ * offPIM Server UI
+ *
+ */
 
 var appUI = express();
 
@@ -39,8 +53,43 @@ appUI.get('/', function(req, res) {
 })
 
 appUI.use('/static', express.static(path.join(__dirname, 'public')))
-appUI.listen(listeningPortoffPIMUI);
 
+appUI.get('/config', function (req, res) {
+    // TODO Fetch either here or frontend UI
+    res.json({
+        bindAddress: configuredBindAddress,
+        dbServerURL: dbServerURL,
+    })
+})
+
+appUI.get('/action/restart', async function (req, res) {
+    await dbServerStop(true);
+    res.json({
+        restart: true,
+        message: 'Restart successful'
+    })
+})
+
+appUI.get('/action/shutdown', async function (req, res) {
+    await dbServerStop();
+    res.json({
+        message: 'Stopped db server'
+    })
+})
+
+appUI.get('/action/start', async function (req, res) {
+    await dbServerStart();
+    res.json({
+        start: true,
+        message: 'Starting db server'
+    })
+})
+
+var uiServer;
+uiServer = appUI.listen(listeningPortoffPIMUI, configuredBindAddress);
 const appUIURL = appURLBase + listeningPortoffPIMUI;
-
 console.log(`offPIM Friendly Interface is available on ${appUIURL}`)
+
+
+dbServerStart();
+open(appUIURL);
